4a06b98c426395d809743960e74e76b2a777a8bb
Twinkle gupta <gtwinkle132@gmail.com>
2020-10-30 23:03:07 +0530

    Update TSP_ACO_GA.py

--- TSP_ACO_GA.py
+++ TSP_ACO_GA.py
@@ -224,3 +224,31 @@ def ga():
             self.edges = edges
             self.tour = None
             self.distance = 0.0
+        def _select_node(self):
+            roulette_wheel = 0.0
+            unvisited_nodes = [node for node in range(self.num_nodes) if node not in self.tour]
+            heuristic_total = 0.0
+            for unvisited_node in unvisited_nodes:
+                heuristic_total += self.edges[self.tour[-1]][unvisited_node].weight
+            for unvisited_node in unvisited_nodes:
+                roulette_wheel += pow(self.edges[self.tour[-1]][unvisited_node].pheromone, self.alpha) * \
+                                  pow((heuristic_total / self.edges[self.tour[-1]][unvisited_node].weight), self.beta)
+            random_value = random.uniform(0.0, roulette_wheel)
+            wheel_position = 0.0
+            for unvisited_node in unvisited_nodes:
+                wheel_position += pow(self.edges[self.tour[-1]][unvisited_node].pheromone, self.alpha) * \
+                                  pow((heuristic_total / self.edges[self.tour[-1]][unvisited_node].weight), self.beta)
+                if wheel_position >= random_value:
+                    return unvisited_node
+
+        def find_tour(self):
+            self.tour = [random.randint(0, self.num_nodes - 1)]
+            while len(self.tour) < self.num_nodes:
+                self.tour.append(self._select_node())
+            return self.tour
+
+        def get_distance(self):
+            self.distance = 0.0
+            for i in range(self.num_nodes):
+                self.distance += self.edges[self.tour[i]][self.tour[(i + 1) % self.num_nodes]].weight
+            return self.distance
